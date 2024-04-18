aff2784b0776733914e5ef09ae10b158795060fb
Ricardo Carneiro <ricadroroberto@outlook.com>
2024-04-18 01:12:31 -0300

    feat: PolicicallyExposed

--- Contracts/ITrade.cs
+++ Contracts/ITrade.cs
@@ -11,6 +11,7 @@ namespace TradeAdmissionTest.Contracts
         double Value { get; } //indicates the transaction amount in dollars
         string ClientSector { get; } //indicates the client´s sector which can be "Public" or "Private"
         DateTime NextPaymentDate { get; } //indicates when the next payment from the client to the bank is expected
+        bool IsPoliticallyExposed { get; }
         ICategory Category { get; }
     }
 }
--- Trade.cs
+++ Trade.cs
@@ -19,12 +19,15 @@ namespace TradeAdmissionTest
 
         public ICategory Category { get; private set; }
 
+        public bool IsPoliticallyExposed { get; private set; }
+
         public Trade(string line, DateTime refDate)
         {
             string[] cols = SliptAndValidate(line);
             Value = double.Parse(cols[0]);
             ClientSector = cols[1];
             NextPaymentDate = DateTime.Parse(cols[2], new CultureInfo("en-US"));
+            IsPoliticallyExposed = bool.Parse(cols[3]);
             Category = CategoryFactory.CreateCategory(this, refDate);
         }
 
@@ -35,11 +38,13 @@ namespace TradeAdmissionTest
                 string[] cols = line.Split(' ');
                 double testValue;
                 DateTime dateNext;
+                bool isPolitically;
                 //It was better if I had a Value Object for each property, but I will follow the instructions
                 if (cols.Any(s => s == null)) throw new ArgumentException("The line was invalid. Sample: 2000000 Private 12/29/2025");
-                if (cols.Length != 3) throw new ArgumentException("The line was invalid. Please use spaces between data. Sample: 2000000 Private 12/29/2025");
+                if (cols.Length != 4) throw new ArgumentException("The line was invalid. Please use spaces between data. Sample: 2000000 Private 12/29/2025");
                 if (!double.TryParse(cols[0], out testValue)) throw new ArgumentException("The line was invalid. The first col must be a number.");
-                if (!DateTime.TryParse(cols[2], new CultureInfo("en-US"), out dateNext)) throw new ArgumentException("The line was invalid. The last col must be a datetime in american format.");
+                if (!DateTime.TryParse(cols[2], new CultureInfo("en-US"), out dateNext)) throw new ArgumentException("The line was invalid. The col must be a datetime in american format.");
+                if (!bool.TryParse(cols[3], out isPolitically)) throw new ArgumentException("The line was invalid. The last col must be a bool (true/false).");
                 return cols;
             }
             catch (ArgumentException ex)
